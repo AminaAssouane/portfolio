dd225344e22e5e30314f703bc768a8dd4eafdec1
Amina <minassouane@gmail.com>
2026-04-04 23:26:30 +0100

    feat: add skills data

---
+++ src/data/skills.js
@@ -0,0 +1,19 @@
+import reactLogo from "../assets/skills/React.svg.png";
+import nodeLogo from "../assets/skills/Node.svg.png";
+import postgresLogo from "../assets/skills/Postgresql.webp.png";
+import javascriptLogo from "../assets/skills/Javascript.png";
+import htmlLogo from "../assets/skills/HTML.svg.png";
+import cssLogo from "../assets/skills/CSS.svg.png";
+import expressLogo from "../assets/skills/Expres.png";
+import prismaLogo from "../assets/skills/Prisma.svg";
+
+export const skills = [
+  { name: "Javascript", icon: javascriptLogo },
+  { name: "React", icon: reactLogo },
+  { name: "HTML", icon: htmlLogo },
+  { name: "CSS", icon: cssLogo },
+  { name: "Express.JS", icon: expressLogo },
+  { name: "Node.JS", icon: nodeLogo },
+  { name: "PostgreSQL", icon: postgresLogo },
+  { name: "Prisna", icon: prismaLogo },
+];
