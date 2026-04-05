e5b77754fa8822624e206fa232fac459b94e8982
Amina <minassouane@gmail.com>
2026-04-05 22:48:04 +0100

    feat: delete theme swticher

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,9 +4,7 @@ import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
 export default function NavBar({ onAbout, onSkills, onProjects, onContact }) {
   return (
     <header className={styles.header}>
-      <div className={styles.left}>
-        <ThemeSwitcher />
-      </div>
+      <div className={styles.left}></div>
       <div className={styles.right}>
         <div onClick={onAbout}>About</div>
         <div onClick={onSkills}>Skills</div>
